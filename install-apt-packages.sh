359d960633c7eae205487c33b5a1cc85c7e0cc29
Cameron Taggart <cameron@tachyus.com>
2016-04-22 21:49:57 -0700

    4 spaces?

--- install-apt-packages.sh
+++ install-apt-packages.sh
@@ -3,9 +3,9 @@ apt-get install -y --no-install-recommends software-properties-common \
 && add-apt-repository -y ppa:ubuntu-toolchain-r/test \
 && apt-get update \
 && apt-get install -y --no-install-recommends \
-  gcc-4.9 \
-  g++-4.9 \
-  gcc-4.9-multilib \
-  g++-4.9-multilib \
-  zip \
-  libgtk2.0-0
+    gcc-4.9 \
+    g++-4.9 \
+    gcc-4.9-multilib \
+    g++-4.9-multilib \
+    zip \
+    libgtk2.0-0
